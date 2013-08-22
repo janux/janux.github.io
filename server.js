fa9778145b428965baffa325245e01c08633666a
Philippe Paravicini <philippe@paravicini.com>
2013-08-21 17:24:55 -0700

    add favicon.ico support and janux icon

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var 
+	  express = require('express')
+	, http    = require('http')
+	, route   = require('./route')
+	, user    = require('./route/user')
+;
+	
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/view');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+app.use(express.favicon());
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', route.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('org-janux server listening on port ' + app.get('port'));
+	console.log('org-janux server listening on port ' + app.get('port'));
 });
